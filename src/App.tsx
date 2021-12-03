e2854c7dae7f022f062762d8d236bf604701143f
AlbertoLutz <albertolutzdias@gmail.com>
2021-12-03 14:07:00 -0300

    Instalando dependências

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 function App() {
 const [count, setCount] = useState(0)
 
+useEffect(() => {
+  if(count >= 10 || count <= -10) {
+    alert(count);
+  }
+})
+
+function handleClick() {
+ setCount(count + 1)
+}
+
+function handleDown() {
+  setCount(count - 1)
+}
+
   return (
     <div className="App">
       <p>{count}</p>
-    <button>Clique aqui</button>
+    <button onClick={handleClick}>Clique aqui +</button>
+    <button onClick={handleDown}>Clique aqui -</button>
     </div>
   );
 }
